d03859a8bee58cbb3c36b4e6bfde231a3dd77755
Surendra kumar pasi <surendramoglaha@gmail.com>
2026-02-14 19:18:06 +0545

    add files

---
+++ 12_See_Variable_Address_Dev_Tools/script.js
@@ -0,0 +1,13 @@
+const firstName = "surendra";
+const firstName1 = "surendra";
+const firstName2 = "suren" + "dra";
+
+
+const age = "21";
+const gender = "Male";
+const country = "India";
+const DOB = "20002";
+const isGraduate = "true";
+const myNull = null;
+const myUndefined = undefined;
+const myEmptyString = "";
